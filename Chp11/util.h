7ef78c9787756b8e46042dcb4a1f787d4919caeb
jinhyung999 <jinju8640108@naver.com>
2024-05-24 12:29:22 +0900

    finish

--- Chp11/util.h
+++ Chp11/util.h
@@ -6,37 +6,38 @@
 #include <algorithm>
 #include <iomanip>
 #include <iostream>
-#include <vector>
+//
 
 #include "analysis.h"
 #include "Student_info.h"
+#include "Vec.h"//Vec >vec
 
 int printGreeting();
 void printMenu();
 
 void loadGrades(ifstream&, 
-	vector<Student_info>&,
-	vector<Student_info>&, 
-	vector<Student_info>&,
+	Vec<Student_info>&,
+	Vec<Student_info>&, 
+	Vec<Student_info>&,
 	string::size_type&
 );
 
 void fileSelect(int,
-	vector<Student_info>&,
-	vector<Student_info>&,
-	vector<Student_info>&,
+	Vec<Student_info>&,
+	Vec<Student_info>&,
+	Vec<Student_info>&,
 	string::size_type&
 );
 
-int checkHw(vector<Student_info>&, vector<Student_info>&);
+int checkHw(Vec<Student_info>&, Vec<Student_info>&);
 
-// Overloaded from analysis.h - to handle a single vector<Student_info> (not two)
+// Overloaded from analysis.h - to handle a single Vec<Student_info> (not two)
 void write_analysis(ostream&, const string&,
-	double analysis(const vector<Student_info>&),
-	const vector<Student_info>&,
+	double analysis(const Vec<Student_info>&),
+	const Vec<Student_info>&,
 	string);
 
-void analyzeGrades(vector<Student_info>&, vector<Student_info>&);
-void showScores(vector<Student_info>&, string::size_type&);
+void analyzeGrades(Vec<Student_info>&, Vec<Student_info>&);
+void showScores(Vec<Student_info>&, string::size_type&);
 
 #endif
